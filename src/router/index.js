501d7437f450fe5b654306da87b63d9827df125b
“lijiesheng” <ljs_hsm@163.com>
2023-02-11 17:19:16 +0800

    挂在路由导航守卫:如果用户没有登录，但是直接通过URL访问特定页面，需要重新导航到登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import HomeVue from '../components/HomeVue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/login',
@@ -24,3 +24,28 @@ export default new Router({
     }
   ]
 })
+
+// 挂在路由导航守卫
+// 功能: 如果用户没有登录，但是直接通过URL访问特定页面，需要重新导航到登录页面
+// to 要访问的路径
+// from 代表从哪个路径跳转过来
+// next 是一个函数, 表示放行
+//   1、next() 放行
+//   2、next('/aaa')  跳转到/aaa路由
+router.beforeEach((to, form, next) => {
+  // 如果用户访问登录页，直接放行
+  console.log(to)
+  if (to.path === '/login') {
+    return next()
+  }
+  // 如果不是，从sessionStorage 中获取保存的token值
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) {
+    console.log('没有获取到', tokenStr)
+    return next('/login') // 跳转到登录页
+  }
+  // 获取到了，放行
+  next()
+})
+
+export default router
