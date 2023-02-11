ac185d9dd7ed260fb2e74220e4e41c46741e4d7f
“lijiesheng” <ljs_hsm@163.com>
2023-02-11 17:27:04 +0800

    退出

--- src/components/HomeVue.vue
+++ src/components/HomeVue.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     这是 homeVue 页面
+    <el-button type="info" @click="logOut">退出</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeVue'
+  name: 'HomeVue',
+  methods: {
+    logOut () {
+      window.sessionStorage.removeItem('token')
+      this.$router.push('/login') // 重定向到登录页面
+    }
+  }
 }
 </script>
 
