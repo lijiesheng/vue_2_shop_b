1de130ff92984285f7eb2a66df4ec3fbd70d1b97
“lijiesheng” <ljs_hsm@163.com>
2023-02-11 21:09:04 +0800

    二级菜单

--- src/components/HomeVue.vue
+++ src/components/HomeVue.vue
@@ -13,7 +13,42 @@
       <!--页面主体区域-->
       <el-container>
         <!--侧边栏-->
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="200px">
+          <!--侧边栏菜单区域-->
+          <el-col>
+            <el-menu
+              background-color="#545c64"
+              active-text-color="#ffd04b">
+              <!--一级菜单-->
+              <el-submenu index="1">
+                <!--一级菜单的模板-->
+                <template slot="title">
+                  <!--图标-->
+                  <i class="el-icon-location"></i>
+                  <!--文本-->
+                  <span>导航一</span>
+                </template>
+                <!--二级菜单-->
+                <el-menu-item index="1-4-1">
+                  <template slot="title">
+                    <!--图标-->
+                    <i class="el-icon-location"></i>
+                    <!--文本-->
+                    <span>导航一</span>
+                  </template>
+                </el-menu-item>
+                <el-menu-item index="1-4-1">
+                  <template slot="title">
+                    <!--图标-->
+                    <i class="el-icon-location"></i>
+                    <!--文本-->
+                    <span>导航一</span>
+                  </template>
+                </el-menu-item>
+              </el-submenu>
+            </el-menu>
+          </el-col>
+        </el-aside>
         <!--右侧内容-->
         <el-main>Main</el-main>
       </el-container>
