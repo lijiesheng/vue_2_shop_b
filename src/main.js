cb82c114b342630660b0be0d1502495001cba064
“lijiesheng” <ljs_hsm@163.com>
2023-02-11 21:23:18 +0800

    每次调用API接口之前,将token的值放到头部信息中

--- src/main.js
+++ src/main.js
@@ -11,6 +11,14 @@ Vue.use(ElementUI)
 
 // 配置请求的根路经
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 通过 axios 请求拦截器添加 token, 保证拥有获取数据的权限
+// 这个是在每次请求后端API之前执行的
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  console.log('config = ', config)
+  return config
+})
+
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
