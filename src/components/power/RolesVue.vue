6adcac3ceb011907f06a8aba8348dd64a080e00c
“lijiesheng” <ljs_hsm@163.com>
2023-02-16 19:45:55 +0800

    面包屑

--- src/components/power/RolesVue.vue
+++ src/components/power/RolesVue.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-    roles
+    <!--使用面包屑导航区域-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <!--点击首页 跳转到 /home-->
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+<!--   一个添加角色按钮   el-row 代表一行 el-col代表一列 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary" round>添加角色</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'RolesVue'
+  name: 'RolesVue',
+  data () {
+  }
 }
 </script>
 
 <style scoped>
+. el-button {
+}
 
+.el-card {
+  margin-top: 20px;
+}
 </style>
