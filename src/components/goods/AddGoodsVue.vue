299eadbf308dc558b10992e59e990d7725b2537c
“lijiesheng” <ljs_hsm@163.com>
2023-02-26 22:34:34 +0800

    步骤条

--- src/components/goods/AddGoodsVue.vue
+++ src/components/goods/AddGoodsVue.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    这是 AddGoodsVue 页面
+    <!--使用面包屑导航区域-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <!--点击首页 跳转到 /home-->
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>添加商品</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--element-ui Card 卡片-->
+    <el-card class="box-card">
+      <!--  :closable="false" 不能关闭    -->
+      <el-alert type="warning" description="添加商品信息" show-icon :closable="false"></el-alert>
+      <!--element-ui Steps 步骤条-->
+    </el-card>
   </div>
 </template>
 
@@ -11,5 +24,11 @@ export default {
 </script>
 
 <style scoped>
+.box-card {
+  margin-top: 25px;
+}
+.el-alert {
+  height: 40px;
+}
 
 </style>
