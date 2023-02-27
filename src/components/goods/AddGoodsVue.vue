ca2a6b488e14f1873ba17d51b73aba83c993a8b2
“lijiesheng” <ljs_hsm@163.com>
2023-02-27 19:05:46 +0800

    tabs的渲染

--- src/components/goods/AddGoodsVue.vue
+++ src/components/goods/AddGoodsVue.vue
@@ -13,22 +13,51 @@
       <!--  :closable="false" 不能关闭    -->
       <el-alert type="warning" description="添加商品信息" show-icon :closable="false"></el-alert>
       <!--element-ui Steps 步骤条-->
+<!--    active 激活下面的那个  0代表第一个-->
+      <el-steps :space="200" :active="activeIndex" finish-status="success" align-center>
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
+
+<!--  Tabs 标签页 -->
+      <el-tabs :tab-position="'left'" style="height: 250px;">
+        <el-tab-pane label="基本信息">基本信息</el-tab-pane>
+        <el-tab-pane label="商品参数">商品参数</el-tab-pane>
+        <el-tab-pane label="商品属性">商品属性</el-tab-pane>
+        <el-tab-pane label="商品图片">商品图片</el-tab-pane>
+        <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+        <el-tab-pane label="完成">完成</el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AddGoodsVue'
+  name: 'AddGoodsVue',
+  data () {
+    return {
+      activeIndex: 0
+    }
+  }
 }
 </script>
 
 <style scoped>
 .box-card {
   margin-top: 25px;
+  height: 900px;
 }
 .el-alert {
   height: 40px;
+}
+.el-steps {
+  /*height: 100px;*/
+  margin: 15px 0;
 }
 
 </style>
